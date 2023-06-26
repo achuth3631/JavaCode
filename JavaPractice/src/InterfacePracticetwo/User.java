867fac52fb2c09623294c51660a81f5fec9624dd
achut <achut@LAPTOP-M0LJ28DQ>
2023-06-26 16:35:30 -0500

    first commit

--- JavaPractice/src/InterfacePracticetwo/User.java
+++ JavaPractice/src/InterfacePracticetwo/User.java
@@ -3,6 +3,12 @@ package InterfacePracticetwo;
 public class User {
 
 	public static void main(String[] args) {
+		Lenovo ln = new Lenovo();
+		ln.camera();
+		ln.copy();
+		ln.cut();
+		ln.paste();
+		ln.keyboard();
 
 	}
 
